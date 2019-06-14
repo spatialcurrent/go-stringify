e36c18d9175297693c763b4385aca41c9c72400d
pjdufour <pjdufour.dev@gmail.com>
2019-06-14 09:22:21 -0400

    concat function

---
+++ pkg/stringify/Concat_test.go
@@ -0,0 +1,45 @@
+// =================================================================
+//
+// Copyright (C) 2019 Spatial Current, Inc. - All Rights Reserved
+// Released as open source under the MIT License.  See LICENSE file.
+//
+// =================================================================
+
+package stringify
+
+import (
+	"testing"
+)
+
+import (
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConcat(t *testing.T) {
+	in := []interface{}{
+		[]interface{}{
+			"a",
+			"b",
+			"c",
+			[]interface{}{
+				"d",
+				"e",
+				"f",
+			},
+			[]interface{}{
+				"g",
+				"h",
+				"i",
+			},
+			"j",
+			"k",
+		},
+		"l",
+		"m",
+		"n",
+	}
+	stringer := NewStringer("-", false, false, false)
+	str, err := Concat(in, stringer)
+	assert.NoError(t, err)
+	assert.Equal(t, str, "abcdefghijklmn")
+}
